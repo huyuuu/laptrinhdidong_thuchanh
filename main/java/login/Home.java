fa5b02c93ecdfcb33276dbe0f6363fc95788794f
huyuuu <91888247+huyuuu@users.noreply.github.com>
2022-04-05 12:21:50 +0700

    Add files via upload

---
+++ main/java/login/Home.java
@@ -0,0 +1,38 @@
+package sv61130384.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    private TextView tvtitle, tvdes, tvmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        tvtitle = (TextView) findViewById(R.id.tv_title);
+        tvdes = (TextView) findViewById(R.id.tv_des);
+        tvmail = (TextView) findViewById(R.id.tv_mail);
+
+        setDataByExtras();
+
+    }
+
+    public void setDataByExtras()
+    {
+        Intent intent = getIntent();
+        String title = intent.getStringExtra(Login.TITLE);
+        String des = intent.getStringExtra(Login.DES);
+        String mail = intent.getStringExtra(Login.MAIL);
+
+        tvtitle.setText(title);
+        tvdes.setText(des);
+        tvmail.setText(mail);
+    }
+}
---
+++ main/java/login/MainActivity.java
@@ -0,0 +1,20 @@
+package sv61130384.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ChuyenManHinh(View v){
+        Intent iLogin= new Intent(this, Login.class);
+        startActivity(iLogin);
+    }
+}
